056b8cc52c49ecf04a8927a623ee4f0bbad68444
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-01 07:29:04 +0000

    complete debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,11 +17,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render 'index', user: @user
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   #get 'home/about', to: 'homes#about', as: :about_path
-  get 'about' => 'homes#about'
+  get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
 end
